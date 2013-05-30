c8f83ac7c83d5b2da8265aec2a9947601a2a7fbd
Smirnov Ruslan <smirnov@realweb.ru>
2013-05-30 13:17:32 +0400

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Author: G@mOBEP
+ *
+ * Date: 20.04.13
+ * Time: 16:40
+ */
+
+exports.IssetValidator      = require('./lib/issetValidator').IssetValidator;
+exports.TypeValidator       = require('./lib/typeValidator').TypeValidator;
+exports.TypesArrayValidator = require('./lib/typesArrayValidator').TypesArrayValidator;
+exports.ValidatorList       = require('./lib/validatorList').ValidatorList;
