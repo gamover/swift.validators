bb3c0a7e8e76c31d9d65f393aeca1dcec967d3f8
Smirnov Ruslan <smirnov@realweb.ru>
2013-06-05 10:54:19 +0400

    sc

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
  * Time: 16:40
  */
 
+exports.EmailValidator      = require('./lib/emailValidator').EmailValidator;
+exports.EqualValidator      = require('./lib/equalValidator').EqualValidator;
 exports.IssetValidator      = require('./lib/issetValidator').IssetValidator;
+exports.NotEmptyValidator   = require('./lib/notEmptyValidator').NotEmptyValidator;
 exports.TypeValidator       = require('./lib/typeValidator').TypeValidator;
 exports.TypesArrayValidator = require('./lib/typesArrayValidator').TypesArrayValidator;
 exports.ValidatorList       = require('./lib/validatorList').ValidatorList;
